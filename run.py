db48005048f3edf04dc65b9e8c193e50aedc993e
Alexander Baker <alex.jbaker20@googlemail.com>
2024-08-18 13:41:49 +0000

    Maint - add comments to boards & sea function

--- run.py
+++ run.py
@@ -2,19 +2,31 @@ from colorama import Fore, Back, Style
 import board
 from random import randrange
 
+
+"""
+define ship and hit icons for visual indicator on maps
+"""
 ships = ["B"]
 hit = ["X"]
 
 def bsea():
+"""
+function to fill empty board with 'sea' icons for user board
+"""
     import random, string
     while True:
         yield random.choice("~")
 
+
 def csea():
+"""
+function to fill empty board with 'sea' icons for computer and dummy board
+"""
     import random, string
     while True:
         yield random.choice("-")
 
+# define small/medium/large user boards & populate grid with bsea background
 bsmall = board.Board((8, 8))
 bsmall.populate(bsea())
 
@@ -24,6 +36,7 @@ bmed.populate(bsea())
 blarge = board.Board((12, 12))
 blarge.populate(bsea())
 
+# define small/medium/large computer boards & populate grid with csea background
 csmall = board.Board((8, 8))
 csmall.populate(csea())
 
@@ -33,6 +46,8 @@ cmed.populate(csea())
 clarge = board.Board((12, 12))
 clarge.populate(csea())
 
+# define small/medium/large dummy boards & populate grid with csea background
+# dummy board for visual indication of user attack without showing comp positions
 dsmall = board.Board((8, 8))
 dsmall.populate(csea())
 
@@ -41,3 +56,4 @@ dmed.populate(csea())
 
 dlarge = board.Board((12, 12))
 dlarge.populate(csea())
+
