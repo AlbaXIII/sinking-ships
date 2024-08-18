5fcd919770520e6a2a72b4fbbc6e78b1314c38c1
Alexander Baker <alex.jbaker20@googlemail.com>
2024-08-18 13:31:33 +0000

    Feat - define size & fill for user/comp/dummy boards

--- run.py
+++ run.py
@@ -5,3 +5,39 @@ from random import randrange
 ships = ["B"]
 hit = ["X"]
 
+def bsea():
+    import random, string
+    while True:
+        yield random.choice("~")
+
+def csea():
+    import random, string
+    while True:
+        yield random.choice("-")
+
+bsmall = board.Board((8, 8))
+bsmall.populate(bsea())
+
+bmed = board.Board((10, 10))
+bmed.populate(bsea())
+
+blarge = board.Board((12, 12))
+blarge.populate(bsea())
+
+csmall = board.Board((8, 8))
+csmall.populate(csea())
+
+cmed = board.Board((10, 10))
+cmed.populate(csea())
+
+clarge = board.Board((12, 12))
+clarge.populate(csea())
+
+dsmall = board.Board((8, 8))
+dsmall.populate(csea())
+
+dmed = board.Board((10, 10))
+dmed.populate(csea())
+
+dlarge = board.Board((12, 12))
+dlarge.populate(csea())
